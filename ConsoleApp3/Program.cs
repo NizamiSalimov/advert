0faced2632b64ba4b0ea3077def6c42ad41956fc
nizam <nizam@DESKTOP-TT8HCUN>
2023-03-05 22:44:00 +0400

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded : ");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            if (eded % 2 == 0)   //Daxil edilen eded cutduse
+            {
+                Console.WriteLine("Daxil etdiyiniz eded cut");
+            }
+            else
+            {
+                Console.WriteLine("Daxil etdiyiniz eded tek");
+            }
+            Console.ReadKey();
+        }
+    }
+}
